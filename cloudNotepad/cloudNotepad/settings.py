edbe32f97bb8c4116075f56c27761f217c8445ca
Henry Co <henryco4388@gmail.com>
2020-11-09 19:11:28 -0800

    changed db to postgresql

--- cloudNotepad/cloudNotepad/settings.py
+++ cloudNotepad/cloudNotepad/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .ignore import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'cloudNotepad.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config.NAME,                      
+        'USER': config.USER,
+        'PASSWORD': config.PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
