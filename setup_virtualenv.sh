dc81b6df48626d95278a96c394d93411fd6d8d6d
Henry Co <henryco4388@gmail.com>
2020-11-09 16:34:39 -0800

    django 3.1.3 install on virtualenv

---
+++ setup_virtualenv.sh
@@ -0,0 +1,14 @@
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/development_environment
+
+#======================================
+# install virtualenv and initialize env
+#======================================
+# sudo pip3 install virtualenvwrapper
+# mkvirtualenv cloudNotepad-env
+# workon
+
+#===================
+# install prereqs
+#===================
+# pip3 install django~=3.1.3
+
