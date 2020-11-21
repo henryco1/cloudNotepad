db44e049773aaeb48868e799177bbc31b6b92203
Henry Co <henryco4388@gmail.com>
2020-11-21 13:28:36 -0800

    functional test outline added

--- cloudNotepad/func_tests.py
+++ cloudNotepad/func_tests.py
@@ -1,8 +1,40 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class VisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-browser.quit()
+    def testUserViewPage(self):
+        # User opens and views the homepage
+        # 1. User sees the browser title
+        # 2. User sees the template list
+        # 3. User sees the items in the template
+        # 4. User sees the textbox for adding template items to the list
+        
+        # 1
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Notes', self.browser.title) 
+        self.fail('Test case incomplete')
+
+    def testUserCreateNote(self):
+        # Create functionality of the textbox
+        # 1. User enters template name
+        # 2. User enters template item textbox
+        # 3. User presses the button that adds the item to the list
+        # 4. The template list populates with the new item
+        # 5. Clicking an element in the template list opens a page with the element details
+        # 6. User template state is saved throughout noncontiguous sessions
+        self.fail('Test case incomplete')
+    
+    def testUserDeleteNote(self):
+        self.fail('Test case incomplete')
+
+    def testUserUpdateNote(self):
+        self.fail('Test case incomplete')
+
+if __name__ == '__main__':
+    unittest.main()
